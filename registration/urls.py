d2c791a4ba454f9e479e7915019ae63d80b53b24
Kumaresan <65583580+kumaresankp@users.noreply.github.com>
2024-10-02 10:28:56 +0000

    upate

--- registration/urls.py
+++ registration/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('search/', views.search_paper_id, name='search'),
 ]
+
+
+urlpatterns+= static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 
